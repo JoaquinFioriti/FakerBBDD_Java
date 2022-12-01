d673434760cde6ae6db93df01a85024c86ec9e38
JoaquinFioriti <joaquinfioritifreelance@gmail.com>
2022-12-01 01:44:47 -0300

    listo

--- TpFinalDDBB/src/main/java/Main.java
+++ TpFinalDDBB/src/main/java/Main.java
@@ -23,7 +23,7 @@ public class Main {
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             con= DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/club","root","");
+                    "jdbc:mysql://localhost:3306/club","root","root");
             stmt=con.createStatement();
 
             /*
